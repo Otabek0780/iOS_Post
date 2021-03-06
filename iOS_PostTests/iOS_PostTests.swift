5a04ed7eca5ef1dd7883d5502edb3546501bf678
Otabek0780 <mrdunacdtm@gmail.com>
2021-03-06 23:36:12 +0900

    Initial Commit

---
+++ iOS_PostTests/iOS_PostTests.swift
@@ -0,0 +1,33 @@
+//
+//  iOS_PostTests.swift
+//  iOS_PostTests
+//
+//  Created by kim jong moon on 06/03/2021.
+//
+
+import XCTest
+@testable import iOS_Post
+
+class iOS_PostTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
